80bf012742417f93876adbc1bd52c209bfcd4cd7
abhinav anshul <abhinavanshul4u@gmail.com>
2019-10-22 15:56:58 +0530

    styling added and application done, final commit

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Titles = () => (
   <div>
-    <h1>Weather</h1>
-    <p>Find out temperature, conditions and more...</p>
+    <h1 className="title-container__title">Weather Info</h1>
+    <h3 className="title-container__subtitle">
+      Find out temperature, conditions and more...
+    </h3>
   </div>
 );
 
