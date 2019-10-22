ca52afbd271dc1de9d260b9527acc9edb05dd2c8
abhinav anshul <abhinavanshul4u@gmail.com>
2019-10-22 15:06:33 +0530

    working application done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = "bc8b1777bd124a4bb25eb8c1196c019c";
+
+//http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=bc8b1777bd124a4bb25eb8c1196c019c&units=metric
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
+  // If we are not using ARROW FUNCTION for the getWeather function then we define function like below...
+  //... because the this keyword looses the bind,and we need to bind it to our function...
+  //... but if we are using arrow function, the function is binded automatically
+  /*
+  constructor() {
+    this.getWeather = this.getWeather.bind(this);
+  }
+  */
+
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`
+    );
+    const data = await api_call.json();
+    console.log(data);
+
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the values"
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Titles.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Titles = () => (
+  <div>
+    <h1>Weather</h1>
+    <p>Find out temperature, conditions and more...</p>
+  </div>
+);
+
+export default Titles;
+
+// class Titles extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Weather</h1>
+//         <p>Find out temperature, conditions and more...</p>
+//       </div>
+//     );
+//   }
+// }
